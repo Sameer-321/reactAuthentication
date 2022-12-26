e458b57781fd22d3bb4ea5a6f5bc4ff2e2654b71
Sameer-321 <snrsameer1@gmail.com>
2022-12-27 03:17:31 +0545

    update on LOGIN PAGE

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,73 +1,51 @@
 import React from 'react';
-import { useRef, useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+// import { Link } from 'react-router-dom';
 
 function Login() {
-  const userRef = useRef();
-  const errRef = useRef();
+  // const userRef = useRef();
+  // const errRef = useRef();
 
-  const [user, setUser] = useState('');
-  const [pwd, setPwd] = useState('');
-  const [errMsg, setErrMsg] = useState('');
-  const [success, setSuccess] = useState(true);
+  const [user, setUser] = useState({
+    username: '',
+    password: '',
+  });
 
   useEffect(() => {
-    userRef.current.focus();
+    // userRef.current.focus();
   }, []);
-  useEffect(() => {
-    setErrMsg('');
-  }, [user, pwd]);
 
-  const handleSubmit = async (e) => {
-    console.log('hello world');
-    setSuccess(true);
+  const changeHandler = (e) => {
+    const { value, name } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.prevent.Default();
   };
 
   return (
     <>
-      {success ? (
-        <section>
-          <h1>YOu are logged in</h1>
-          <Link>just go to home</Link>
-        </section>
-      ) : (
-        <section>
-          <p
-            ref={errRef}
-            className={errMsg ? 'errMsg' : 'offscreen'}
-            aria-live="assertive"
-          >
-            {errMsg}
-          </p>
-          <h1>Sign In</h1>
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="username">Username:</label>
-            <input
-              type="text"
-              id="username"
-              ref={userRef}
-              autoComplete="off"
-              onChange={(e) => {
-                setUser(e.target.value);
-              }}
-              value={user}
-              required
-            />
+      <section>
+        <h1>Sign In</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            name="username"
+            value={user.username}
+            onChange={changeHandler}
+          />
 
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              onChange={(e) => {
-                setUser(e.target.value);
-              }}
-              value={pwd}
-              required
-            />
-            <button type="submit">Sign In</button>
-          </form>
-        </section>
-      )}
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            onChange={changeHandler}
+            value={user.password}
+          />
+          <button type="submit">Sign In</button>
+        </form>
+      </section>
     </>
   );
 }
